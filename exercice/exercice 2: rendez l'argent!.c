12a2e9043fffd33e284b0e5a43fa599c8a10b55e
briscko <brice.alliote@hotmail.com>
2024-03-15 07:43:09 +0100

    exercice deux

--- exercice/exercice 2: rendez l'argent!.c
+++ exercice/exercice 2: rendez l'argent!.c
@@ -15,21 +15,81 @@ donnée, indiquer la monnaie a rendre (en billet de 22,10,5 et piece de 2 et 1 e
 
 int main()
 {
-    double somme_a_payer = 0;  
-    double somme_a_prendre =0;
+    float somme_a_payer = 0;  
+    float somme_a_prendre =0;
+    float somme_a_rendre =0;
+    int piece_de_un = 1;
+    int piece_de_deux = 2;
+    int billet_de_cinq = 5;
+    int billet_de_dix = 10;
+    int billet_de_vingt = 20;
    
 
    //difference(double a, double b)
 
 
 printf("à combien s'élève le montant total de vos achats?\n");
-scanf("%lf", &somme_a_payer);
+scanf("%f", &somme_a_payer);
 printf("combien de monnaie avez vous?");
-scanf("%lf", &somme_a_prendre);
+scanf("%f", &somme_a_prendre);
  double difference = somme_a_prendre -  somme_a_payer;
-printf("%lf de diff",  difference);
+if (somme_a_payer > somme_a_prendre )
+{
+   printf("vous n'avez pas donner assez de monnaies \n");
 
-printf("pour la somme de %lf ,nous vous rendons : %lf euro\n", somme_a_payer, difference);
+}
+else if (somme_a_payer == somme_a_prendre)
+{
+    printf("merci et bonne journée\n");
+    }
+else 
+{
+    printf("pour la somme de %.2f ,nous vous rendons : %.2f euro\n", somme_a_payer, difference);
+
+}
+
+while (difference != 0 )
+{
+    if (difference >20)
+    {
+         difference= difference-billet_de_vingt;
+         printf("voici un billet de 20 +");
+    }
+   else if (difference>=10)
+   {
+       difference = difference-billet_de_dix;
+       printf("voici un billet de 10 +");
+    }
+    else if (difference>=5) 
+    { 
+        difference = difference-billet_de_cinq;
+        printf("voici un billet de 5 ");
+   }
+    else if (difference >= 2)
+   {
+        difference = difference-piece_de_deux;
+        printf("voici une piece  de 2 euro +");
+   }
+    else if (difference >= 1)
+   {
+            difference = difference-piece_de_un;
+            printf("voici une piece  de 1 euro +");
+
+   }
+   else if (difference <=0)
+   {
+    difference = 0;
+         printf("et %f centime", difference);
+   }
+   else
+   {
+    printf("erreur");
+    return 0;
+   }
+}
+
+
+
 
 
     return 0 ;
