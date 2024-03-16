7507adc69471bb6900b88ab05e718bcb9b342928
briscko <brice.alliote@hotmail.com>
2024-03-16 08:06:42 +0100

    exercice 3 sommes de nombres

---
+++ exercice/exercice trois: sommes de nombres.c
@@ -0,0 +1,20 @@
+/*
+EXERCICE C # 3
+[Révision : boucles]
+> Ecrire un programme qui calcule la somme des entiers d'un 
+nombre à un autre (maximum : entr e1 et 1000)
+
+> Indications :
+     -prévoir le cas où "min" serait plus grand que "max" et quitter 
+      le programme si ca arrive.
+     -prévoir le cas où "min" serait plus petit que 1 et "max" 
+      plus grand que 1000.
+*/
+
+#include <stdio.h>
+
+
+int main()
+{
+    return 0;
+}
